245b52adc96637b7eba7ea2a230b60d95a773415
nagesh <naagesh2206@gmail.com>
2025-07-28 23:36:51 +0530

    push changes

--- work1/sample1.py
+++
@@ -1,8 +0,0 @@
-import fitz  # PyMuPDF for PDFs
-
-doc = fitz.open("FAQ.pdf")
-text = ""
-for page in doc:
-    text += page.get_text()
-chunks = text.split("\n\n")  # split into paragraphs/questions
-
